0421d67ef38b3964742dc9ae72afad9be7835598
Sadid-E <sethun20@stuy.edu>
2020-12-14 01:53:17 -0500

    Added the add methods

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,9 +1,32 @@
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
 
-  public NoNullArrayList() {
+  public OrderedArrayList() {
   }
 
-  public NoNullArrayList(int startingCapacity) {
+  public OrderedArrayList(int startingCapacity) {
   }
+
+  public int correctIndex(T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("Argument is null, it cannot be null!");
+    }
+    for (int i = 0; i < super.size(); i++) {
+      if (element.compareTo(super.get(i)) < 0) {
+        return i;
+      }
+    }
+    return super.size();
+  }
+
+  public boolean add(T element) {
+    super.add(correctIndex(element), element);
+    return true;
+  }
+
+  public void add(int index, T element) {
+    super.add(correctIndex(element), element);
+  }
+
+
 
 }
