d43a1333396c104bab14184bb31dd343dea4ee64
Sadid-E <sethun20@stuy.edu>
2020-12-14 02:07:28 -0500

    Added the set method

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -27,6 +27,14 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super.add(correctIndex(element), element);
   }
 
-
+  public T set(int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("Argument is null, it cannot be null!");
+    }
+    T original = super.get(index);
+    super.remove(index);
+    super.add(correctIndex(element), element);
+    return original;
+  }
 
 }
